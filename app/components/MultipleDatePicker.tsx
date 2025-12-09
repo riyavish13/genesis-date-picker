eded50c82c15a8d34465d714eb0ff7d75a14fc08
Riya Vishwakarma <riya.vishwakarma@atomostech.com>
2025-12-09 11:13:22 +0530

    forwardRef

--- app/components/MultipleDatePicker.tsx
+++ app/components/MultipleDatePicker.tsx
@@ -9,6 +9,7 @@ import React, {
   useEffect,
   useRef,
   useState,
+  forwardRef,
 } from "react";
 import { DayPicker, DropdownProps, PropsMulti } from "react-day-picker";
 import { cn } from "../utils/utils";
@@ -30,149 +31,150 @@ interface MultipleDatePickerProps {
   min?: number | undefined;
 }
 
-const MultipleDatePicker = ({
-  placeholder = "DD/MM/YYYY",
-  selectedDate,
-  setSelectedDate,
-  footer,
-  position = "bottom-right",
-  disabledCalendar,
-  dateFormat,
-  disabled,
-  hideWeekdays,
-  endMonth,
-  startMonth,
-  timeZone = "Asia/Kolkata",
-  min,
-  max,
-}: MultipleDatePickerProps) => {
-  const [isPopperOpen, setIsPopperOpen] = useState(false);
-  const popperRef = useRef<HTMLDivElement>(null);
-  const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(
-    null
-  );
+const MultipleDatePicker = forwardRef<HTMLInputElement, MultipleDatePickerProps>(
+  (
+    {
+      placeholder = "DD/MM/YYYY",
+      selectedDate,
+      setSelectedDate,
+      footer,
+      position = "bottom-right",
+      disabledCalendar,
+      dateFormat,
+      disabled,
+      hideWeekdays,
+      endMonth,
+      startMonth,
+      timeZone = "Asia/Kolkata",
+      min,
+      max,
+    },
+    ref
+  ) => {
+    const [isPopperOpen, setIsPopperOpen] = useState(false);
+    const popperRef = useRef<HTMLDivElement>(null);
+    const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(
+      null
+    );
 
-  const closePopper = () => setIsPopperOpen(false);
+    const closePopper = () => setIsPopperOpen(false);
 
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        popperElement &&
-        !popperElement.contains(event.target as Node) &&
-        !popperRef.current?.contains(event.target as Node)
-      ) {
-        closePopper();
-      }
+    useEffect(() => {
+      const handleClickOutside = (event: MouseEvent) => {
+        if (
+          popperElement &&
+          !popperElement.contains(event.target as Node) &&
+          !popperRef.current?.contains(event.target as Node)
+        ) {
+          closePopper();
+        }
+      };
+
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [popperElement]);
+
+    const formatSelectedDate = (dates?: Date[], dateFormat?: string) => {
+      if (!dates || dates.length === 0) return "";
+      return dates.map((d) => format(d, dateFormat || "dd/MM/yyyy")).join(", ");
     };
 
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [popperElement]);
+    function CustomSelectDropdown(props: DropdownProps) {
+      const { options, value, onChange } = props;
 
-  const formatSelectedDate = (dates?: Date[], dateFormat?: string) => {
-    if (!dates || dates.length === 0) return "";
-    return dates.map((d) => format(d, dateFormat || "dd/MM/yyyy")).join(", ");
-  };
+      return (
+        <select
+          className="border p-1 shadow rounded-md mb-3 outline-none mx-1"
+          value={value?.toString()}
+          onChange={(e) => onChange?.(e)}
+        >
+          {options?.map((option) => (
+            <option
+              key={option.value}
+              value={option.value.toString()}
+              disabled={option.disabled}
+            >
+              {option.label}
+            </option>
+          ))}
+        </select>
+      );
+    }
 
-  function CustomSelectDropdown(props: DropdownProps) {
-    const { options, value, onChange } = props;
-
-    const handleValueChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-      if (onChange) {
-        onChange(event);
-      }
+    const handleDaySelect: PropsMulti["onSelect"] = (date) => {
+      if (date) setSelectedDate(date);
     };
 
     return (
-      <select
-        className="border p-1 shadow rounded-md mb-3 outline-none mx-1"
-        value={value?.toString()}
-        onChange={handleValueChange}
-      >
-        {options?.map((option) => (
-          <option
-            key={option.value}
-            value={option.value.toString()}
-            disabled={option.disabled}
-          >
-            {option.label}
-          </option>
-        ))}
-      </select>
-    );
-  }
-
-  const handleDaySelect: PropsMulti["onSelect"] = (date) => {
-    if (date) {
-      setSelectedDate(date);
-      // closePopper();
-    }
-  };
-  return (
-    <div className="relative w-full">
-      <div ref={popperRef}>
-        <Input
-          type="text"
-          readOnly
-          className="w-full main-shadow"
-          placeholder={placeholder || format(new Date(), "dd/mm/yyyy")}
-          aria-label="Pick a date"
-          value={formatSelectedDate(selectedDate, dateFormat)}
-          onClick={() => setIsPopperOpen(true)}
-          disabled={disabled}
-          startIcon={<RiCalendarLine size={16} />}
-        />
-      </div>
-      {isPopperOpen && (
-        <div
-          tabIndex={-1}
-          className={cn(
-            "text-[16px] shadow-sm border bg-white rounded-md",
-            "mt-1 mx-auto z-[1000] transition-all absolute duration-75 delay-100 ease-in-out",
-            {
-              "bottom-11 right-0": position === "top-right",
-              "bottom-11 left-0": position === "top-left",
-              "top-10 right-0": position === "bottom-right" || !position,
-              "top-10 left-0": position === "bottom-left",
-            }
-          )}
-          ref={(element) => setPopperElement(element)}
-          role="dialog"
-          aria-label="Multiple DayPicker calendar"
-        >
-          <DayPicker
-            mode="multiple"
-            hideNavigation
-            hideWeekdays={hideWeekdays}
-            showOutsideDays
-            startMonth={startMonth}
-            endMonth={
-              endMonth ? endMonth : new Date(new Date().getFullYear() + 100, 12)
-            }
-            selected={selectedDate}
-            timeZone={timeZone}
-            max={max}
-            min={min}
-            defaultMonth={selectedDate?.[0] || new Date()}
-            disabled={disabledCalendar}
-            components={{ Dropdown: CustomSelectDropdown }}
-            captionLayout="dropdown"
-            onSelect={handleDaySelect}
-            modifiersStyles={{
-              selected: {
-                backgroundColor: "var(--primary-500)",
-                color: "white",
-                borderRadius: "5px",
-              },
-            }}
-            footer={footer}
+      <div className="relative w-full">
+        <div ref={popperRef}>
+          <Input
+            type="text"
+            readOnly
+            ref={ref}
+            className="w-full main-shadow"
+            placeholder={placeholder || format(new Date(), "dd/mm/yyyy")}
+            aria-label="Pick a date"
+            value={formatSelectedDate(selectedDate, dateFormat)}
+            onClick={() => setIsPopperOpen(true)}
+            disabled={disabled}
+            startIcon={<RiCalendarLine size={16} />}
           />
         </div>
-      )}
-    </div>
-  );
-};
+
+        {isPopperOpen && (
+          <div
+            tabIndex={-1}
+            className={cn(
+              "text-[16px] shadow-sm border bg-white rounded-md",
+              "mt-1 mx-auto z-[1000] transition-all absolute duration-75 delay-100 ease-in-out",
+              {
+                "bottom-11 right-0": position === "top-right",
+                "bottom-11 left-0": position === "top-left",
+                "top-10 right-0": position === "bottom-right" || !position,
+                "top-10 left-0": position === "bottom-left",
+              }
+            )}
+            ref={(element) => setPopperElement(element)}
+            role="dialog"
+            aria-label="Multiple DayPicker calendar"
+          >
+            <DayPicker
+              mode="multiple"
+              hideNavigation
+              hideWeekdays={hideWeekdays}
+              showOutsideDays
+              startMonth={startMonth}
+              endMonth={
+                endMonth ? endMonth : new Date(new Date().getFullYear() + 100, 12)
+              }
+              selected={selectedDate}
+              timeZone={timeZone}
+              max={max}
+              min={min}
+              defaultMonth={selectedDate?.[0] || new Date()}
+              disabled={disabledCalendar}
+              components={{ Dropdown: CustomSelectDropdown }}
+              captionLayout="dropdown"
+              onSelect={handleDaySelect}
+              modifiersStyles={{
+                selected: {
+                  backgroundColor: "var(--primary-500)",
+                  color: "white",
+                  borderRadius: "5px",
+                },
+              }}
+              footer={footer}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
+);
+
+MultipleDatePicker.displayName = "MultipleDatePicker";
 
 export default MultipleDatePicker;
